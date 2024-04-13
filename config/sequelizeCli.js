77bbab94373a565347b2d6ea0ef80a2e6f76f05e
Guilherme Serafim Kollet <40221574+guilhermekollet@users.noreply.github.com>
2024-04-13 16:06:38 -0300

    init

---
+++ config/sequelizeCli.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'kolex_development',
+        username: 'kolex',
+        password: 'kolex'
+}
